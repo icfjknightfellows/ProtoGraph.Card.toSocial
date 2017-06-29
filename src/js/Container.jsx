6dfe3201312c5ddfe3a405607361c2cafec434dc
abha-m <abhamutalik@gmail.com>
2017-06-29 14:05:47 +0530

    updated renderScreenshot

--- src/js/Container.jsx
+++ src/js/Container.jsx
@@ -120,10 +120,11 @@ export default class ShareCard extends React.Component {
         }
       }
       return (
-        <div>
-          {cover_image &&
-            (this.props.mode === "instagram") ? <img className="proto-cover-insta-screenshot" style = {styles} src = {cover_image}/> : <img className="proto-cover-fb-screenshot" style = {styles} src = {cover_image}/>
+        <div id="ProtoScreenshot">
+          {
+            (this.props.mode === "instagram") ? <img className="proto-cover-insta-screenshot" style = {styles} src = {cover_image}/> : ((this.props.mode === "facebook")? <img className="proto-cover-fb-screenshot" style = {styles} src = {cover_image}/> : <img src="https://static.pexels.com/photos/1510/road-sky-sand-street.jpg"/>)
           }
+
         </div>
       )
     }
